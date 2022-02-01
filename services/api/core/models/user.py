57914e46f1f0588118245e6a9bc0dc57586f5eb9
olfredos6 <alfred.nehemie@gmail.com>
2022-02-01 08:29:04 +0200

    django baare user api

--- services/api/core/models/user.py
+++ services/api/core/models/user.py
@@ -9,7 +9,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser, PermissionsMixin):
-    pass
             # username_validator = ASCIIUsernameValidator()
             # username = CICharField(
             #     _("username"),
@@ -22,53 +21,53 @@ class User(AbstractUser, PermissionsMixin):
             #     },
             # )
             # first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    # last_name = models.CharField(_("last name"), max_length=150, blank=True)
-    # email = CIEmailField(
-    #     _("email address"),
-    #     unique=True,
-    #     error_messages={
-    #         "unique": _("A user with that email address already exists."),
-    #     },
-    # )
-    # is_staff = models.BooleanField(
-    #     _("staff status"),
-    #     default=False,
-    #     help_text=_("Designates whether the user can log into this admin site."),
-    # )
-    # is_active = models.BooleanField(
-    #     _("active"),
-    #     default=True,
-    #     help_text=_(
-    #         "Designates whether this user should be treated as active. Unselect this instead of deleting accounts."
-    #     ),
-    # )
-    # date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    email = CIEmailField(
+        _("email address"),
+        unique=True,
+        error_messages={
+            "unique": _("A user with that email address already exists."),
+        },
+    )
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
-    # objects = UserManager()
+    objects = UserManager()
 
-    # EMAIL_FIELD = "email"
-    # USERNAME_FIELD = "email"
-    # # REQUIRED_FIELDS = ["email"]
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
-    # class Meta:
-    #     verbose_name = _("user")
-    #     verbose_name_plural = _("users")
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
 
-    # def clean(self):
-    #     super().clean()
-    #     self.email = self.__class__.objects.normalize_email(self.email)
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
 
-    # def get_full_name(self):
-    #     """
-    #     Return the first_name plus the last_name, with a space in between.
-    #     """
-    #     full_name = "%s %s" % (self.first_name, self.last_name)
-    #     return full_name.strip()
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
 
-    # def get_short_name(self):
-    #     """Return the short name for the user."""
-    #     return self.first_name
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
 
-    # def email_user(self, subject, message, from_email=None, **kwargs):
-    #     """Send an email to this user."""
-    #     send_mail(subject, message, from_email, [self.email], **kwargs)
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
